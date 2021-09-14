fc67bc18cc7948a5f87c82bfc973c9340d155bab
fan2level <fan2level@gmail.com>
2021-09-14 17:04:31 +0900

    fix general Tree data structure example

--- basic/tree.py
+++ basic/tree.py
@@ -1,41 +1,43 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
-class Node(object):
-    def __init__(self, data):
-        self.data = data
-        self.__child = list()
+class Tree(object):
+    "Generic tree node."
+    def __init__(self, name='root', children=None):
+        self.name = name
+        self.children = []
+        if children is not None:
+            for child in children:
+                self.add(child)
+    def __repr__(self):
+        return self.name
+    def add(self, node):
+        assert isinstance(node, Tree)
+        self.children.append(node)
 
-    def add(self, data):
-        self.__child.append(data)
-
-    def get(self, data):
-        return next((x for x in self.__child if x == data), None)
+    def _search(self, root, name):
+        if root.name == name:
+            return root
+        for child in root.children:
+            if self._search(child, name) != None:
+                return child
+    def search(self, name):
+        return self._search(self, name)
     
-    def children(self):
-        return self.__child
+    def _print(self, tree, indent):
+        print(f"{'':<{indent}}{tree}")
+        if len(tree.children) == 0:
+            return
+        for child in tree.children:
+            self._print(child, indent+2)
+    def printtree(self):
+        self._print(self, 0)
 
-class LinkedList(object):
-    def __init__(self):
-        self.root = None
-
-    def _printitems(self, child, indent):
-        print(f"{'':<{indent}}{child.data}")
-        for item in child.children():
-            self._printitems(item, indent+2)
-
-    def printitems(self):
-        self._printitems(self.root, 0)
-        
 if __name__ == '__main__':
-    tree = LinkedList()
-    one = Node(1)
-    tree.root = one
-    two = Node(2)
-    three = Node(3)
-    four = Node(4)
-    
-    one.add(two)
-    one.add(three)
-    three.add(four)
+    t = Tree('*', [Tree('1'),
+                   Tree('2'),
+                   Tree('+', [Tree('3'),
+                              Tree('4')])])
 
-    tree.printitems()
+    t.printtree()
+    print()
+    print(t.search('2'))
