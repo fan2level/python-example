5fdd62ff27ea4efb671ebf7000b43b5050a1970e
fan2level <fan2level@gmail.com>
2023-07-19 22:51:02 +0900

    decorate pattern

--- basic/decorate.py
+++ basic/decorate.py
@@ -25,3 +25,18 @@ class Person(object):
 my_person = Person()
 print(my_person.get_fullname())
 
+# --------------------------------------------------
+def tags(tag_name):
+    def tags_decorator(func):
+        def func_wrapper(name):
+            return "<{0}>{1}</{0}>".format(tag_name, func(name))
+        return func_wrapper
+    return tags_decorator
+
+@tags("p")
+@tags("h")
+def get_text(name):
+    return "Hello "+name
+print(get_text("Jone"))
+
+
