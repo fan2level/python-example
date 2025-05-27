2d738a0d79d740929d5b9aa768c41b23a2806bec
fan2level <fan2level@gmail.com>
2025-05-27 16:31:56 +0900

    add jsonschema sample

---
+++ nvd-cve/b0.py
@@ -0,0 +1,23 @@
+import json
+from jsonschema import validate
+from pathlib import Path
+import requests
+from pprint import pprint
+
+# json schema from file
+# json_schema_f = Path("nvd.schema/cve_api_json_2.0.schema.json")
+# with open(json_schema_f, 'r') as f:
+#     json_schema = json.load(f)
+
+# json schema from url
+json_schema_url = "https://csrc.nist.gov/schema/nvd/api/2.0/cve_api_json_2.0.schema"
+response = requests.get(json_schema_url)
+response.raise_for_status()
+json_schema = response.json()
+
+nvd_sample_f = Path("nvd.data/CVE-2018-10902.json")
+with open(nvd_sample_f, 'r') as f:
+    nvd_sample = json.load(f)
+
+validate(instance=nvd_sample, schema=json_schema)
+
